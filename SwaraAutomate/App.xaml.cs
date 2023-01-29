2cec895806cde9f6da8561f044d1075c324eca9c
mrjarvis698 <dudamajaykumar59@gmail.com>
2023-01-29 19:46:46 +0530

    Swara: Automate: Init MAUI Project base.
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

---
+++ SwaraAutomate/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SwaraAutomate;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SwaraAutomate/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SwaraAutomate;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
